2b12022aed3d857adc3fb2845bcc83fd711a3fb0
Rashika258 <rashikasuresh5758@gmail.com>
2024-04-14 07:40:11 +0530

    contact changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,56 +11,64 @@ export default function Contact() {
         text={"Transforming ideas into captivating online experiences."}
       />
 
-      <form action="#" className="max-w-xl space-y-8  py-8">
-        <div>
-          <label
-            htmlFor="email"
-            className="mb-2 block text-sm font-medium  text-gray-300"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="focus:ring-primary-500  focus:border-primary-500   focus:ring-primary-500 focus:border-primary-500 shadow-sm-light block w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white placeholder-gray-400 shadow-sm"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="subject"
-            className="mb-2 block text-sm font-medium text-gray-300"
-          >
-            Subject
-          </label>
-          <input
-            type="text"
-            id="subject"
-            className="focus:ring-primary-500 focus:border-primary-500 focus:ring-primary-500 focus:border-primary-500  shadow-sm-light block  w-full rounded-lg border border-gray-600 bg-gray-700 p-3 text-sm text-white placeholder-gray-400 shadow-sm"
-            placeholder="Let us know how we can help you"
-            required
-          />
-        </div>
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="message"
-            className="mb-2 block text-sm font-medium text-gray-400"
-          >
-            Your message
-          </label>
-          <textarea
-            id="message"
-            rows={6}
-            className="focus:ring-primary-500 focus:border-primary-500 focus:ring-primary-500 focus:border-primary-500 block min-h-[50px] w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white shadow-sm dark:placeholder-gray-400"
-            placeholder="Leave a comment..."
-          ></textarea>
-        </div>
+      <div className="flex w-full items-center justify-center py-8">
+        <form
+          action="mailto:rashikasuresh312@gmail.com"
+          className="w-full  max-w-xl space-y-4  "
+        >
+          <div>
+            <label
+              htmlFor="email"
+              className="mb-2 block text-sm font-medium  text-gray-300"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="focus:ring-primary-500  focus:border-primary-500   focus:ring-primary-500 focus:border-primary-500 shadow-sm-light block w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white placeholder-gray-400 shadow-sm"
+              placeholder="name@youremail.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="subject"
+              className="mb-2 block text-sm font-medium text-gray-300"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              className="focus:ring-primary-500 focus:border-primary-500 focus:ring-primary-500 focus:border-primary-500  shadow-sm-light block  w-full rounded-lg border border-gray-600 bg-gray-700 p-3 text-sm text-white placeholder-gray-400 shadow-sm"
+              placeholder="Let us know how we can help you"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="mb-2 block text-sm font-medium text-gray-400"
+            >
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows={6}
+              className="focus:ring-primary-500 focus:border-primary-500 focus:ring-primary-500 focus:border-primary-500 block min-h-[50px] w-full rounded-lg border border-gray-600 bg-gray-700 p-2.5 text-sm text-white shadow-sm dark:placeholder-gray-400"
+              placeholder="Leave a comment..."
+            ></textarea>
+          </div>
 
-        <button className="rounded-full border border-black bg-white/[0.2] px-4 py-2 text-sm backdrop-blur-sm transition duration-200 hover:shadow-[0px_0px_4px_4px_rgba(0,0,0,0.1)]">
-          Send Message
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="rounded-full border border-black bg-white/[0.2] px-4 py-2 text-sm backdrop-blur-sm transition duration-200 hover:shadow-[0px_0px_4px_4px_rgba(0,0,0,0.1)]"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
