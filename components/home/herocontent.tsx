856e5bd4f944b3058eef1c63df46fb4aea8d66bf
Rashika258 <rashikasuresh5758@gmail.com>
2024-04-14 08:04:35 +0530

    Resume Download added

--- components/home/herocontent.tsx
+++ components/home/herocontent.tsx
@@ -27,14 +27,21 @@ const HeroContent = () => {
     <div className=" relative mb-[80px] mt-[65px] flex   h-[calc(100vh-145px)] w-full flex-col justify-start p-8 xl:items-center  xl:justify-center   xl:p-20 ">
       <div className="flex w-full">
         <div className="hero__image__wrapper relative hidden rounded-2xl border  border-slate-500 shadow-lg md:flex ">
-          <img className="rounded-2xl" src={"/avatar_2.png"} alt="avatar" />
+          <img
+            className="min-w-[300px] rounded-2xl"
+            src={"/avatar_2.png"}
+            alt="avatar"
+          />
         </div>
 
-        <div className=" flex flex-col md:items-center md:justify-center md:py-8 md:pl-16  ">
+        <div className=" flex flex-col md:items-center md:justify-center  md:pl-8  ">
+          <Link href={"/resume.pdf"} className="z-50" target="blank">
           <HeaderText
-            text={"Full Stack Developer Portfolio"}
+            text={"Download Resume"}
             className="pt-0 md:justify-center"
           />
+          </Link>
+  
 
           <p className="flex items-center py-8 text-left text-gray-400 ">
             {heroWords}
@@ -50,6 +57,7 @@ const HeroContent = () => {
               </p>
             </div>
           </div>
+
           <div className="z-50 flex md:hidden">
             {footerSocialLinks &&
               footerSocialLinks?.length > 0 &&
