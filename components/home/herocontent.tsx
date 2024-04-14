74a3a5e01f49b1e1eaf68299f5c347a638d5eae7
Rashika258 <rashikasuresh5758@gmail.com>
2024-04-14 09:47:02 +0530

    -button issue

--- components/home/herocontent.tsx
+++ components/home/herocontent.tsx
@@ -35,7 +35,7 @@ const HeroContent = () => {
         </div>
 
         <div className=" flex flex-col md:items-center md:justify-center  md:pl-8  ">
-          <div className="w-full items-center justify-start">
+          <div className="w-full items-center justify-start z-50">
           <Link href={"/resume.pdf"} className="z-50" target="blank">
             <button className="group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-3xl bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-white hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-800 group-hover:from-purple-600  group-hover:to-blue-500">
               <span className="relative rounded-3xl bg-gray-900 px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0">
